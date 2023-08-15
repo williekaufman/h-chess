3c6a7c786a6a26b462ec04557a8c4d263e5ca2e4
Willie <williekaufman1@gmail.com>
2023-08-15 18:24:07 -0400

    x

--- handicaps.py
+++ handicaps.py
@@ -19,12 +19,20 @@ def die_after_moving_pawn(board, start, stop, history):
     last_move = history[-2]
     return not last_move.piece.piece.value == 'P'
 
+def lose_if_no_queen(board, start, stop, history):
+    color = 'W' if not history or history[-1].piece.color.value == 'B' else 'B'
+    for square in Square:
+        if board.get(square) and board.get(square).piece.value == 'Q' and board.get(square).color.value == color:
+            return True
+    return False
+
 # number is how bad the handicap is, 1-10
 handicaps = {
     'No handicap': (no_handicap, 1),
     "Can't move pawns": (cant_move_pawns, 7),
     "Can't move pawn and then rook": (cant_move_pawn_and_then_rook, 3),
-    "Die after moving pawn": (die_after_moving_pawn, 6),
+    "Die after moving pawn": (die_after_moving_pawn, 7),
+    "Lose if you have no queen": (lose_if_no_queen, 7),
 }
 
 # theoretical args for some kind of config, e.g. difficulties, elos, idk
