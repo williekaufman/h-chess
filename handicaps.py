1798e116bfb3bf8efeb1bb4fc0a3682c42113ae7
Willie <williekaufman1@gmail.com>
2023-08-17 14:54:14 -0400

    x

--- handicaps.py
+++ handicaps.py
@@ -3,11 +3,14 @@ from chess import Color, Piece, ColoredPiece
 from settings import LOCAL
 import random
 
+
 def no_handicap(board, start, stop, history):
     return True
 
+
 def cant_move_pawns(board, start, stop, history):
     return not (board.get(start) and board.get(start).piece == Piece.PAWN)
+
 
 def cant_move_pawn_and_then_rook(board, start, stop, history):
     history = history.history
@@ -16,12 +19,14 @@ def cant_move_pawn_and_then_rook(board, start, stop, history):
     last_move = history[-2]
     return not (board.get(start) and board.get(start).piece == Piece.ROOK and last_move.piece.piece == Piece.PAWN)
 
+
 def die_after_moving_pawn(board, start, stop, history):
     history = history.history
     if len(history) < 2:
         return True
     last_move = history[-2]
     return not last_move.piece.piece.value == 'P'
+
 
 def lose_if_no_queen(board, start, stop, history):
     return board.loc(ColoredPiece(history.whose_turn(), Piece.QUEEN))
@@ -33,15 +38,18 @@ def skittish(board, start, stop, history):
         print(history[-1].check)
     return not history or not history[-1].check or board.get(start).piece == Piece.KING
 
+
 def bongcloud(board, start, stop, history):
     king_pos = board.loc(ColoredPiece(history.whose_turn(), Piece.KING))
     if not king_pos:
         # You should probably have a king but idk not my problem if you don't
         return True
     if king_pos[0].rank() == Rank.First if history.whose_turn() == Color.WHITE else Rank.Eighth:
-        piece = board.get(start) and board.get(start).piece # piece should never be None but idk
+        # piece should never be None but idk
+        piece = board.get(start) and board.get(start).piece
         return piece in [Piece.PAWN, Piece.KING]
-    return True 
+    return True
+
 
 def cant_move_to_opponents_side_of_board(board, start, stop, history):
     color = history.whose_turn()
@@ -49,6 +57,7 @@ def cant_move_to_opponents_side_of_board(board, start, stop, history):
     if color == Color.WHITE:
         ranks = [rank.flip() for rank in ranks]
     return not stop.rank() in ranks
+
 
 # number is how bad the handicap is, 1-10
 handicaps = {
@@ -65,9 +74,11 @@ handicaps = {
 descriptions = {v[0]: k for k, v in handicaps.items()}
 
 # theoretical args for some kind of config, e.g. difficulties, elos, idk
+
+
 def get_handicaps(x, y):
     # So I can't forget to undo anything weird
     if not LOCAL:
-        return random.sample(handicaps.keys(), 2) 
+        return random.sample(handicaps.keys(), 2)
     return descriptions[die_after_moving_pawn], descriptions[lose_if_no_queen]
     # return descriptions[cant_move_to_opponents_side_of_board], descriptions[cant_move_to_opponents_side_of_board]
